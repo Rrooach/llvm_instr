530a36369373bd4227e99ae98328480c154bcbf5
root <root@syzkaller>
2020-05-13 10:16:09 +0000

    cov.c

--- cov.c
+++ cov.c
@@ -5,23 +5,21 @@
 #include <xen/sys/privcmd.h>
 #include <errno.h>
 #include <xen/xen.h>
-
-
-
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define SHIFT 5
+#define MASK 0x1f
 int main(void){
 	int fd, covFd;
 	int ret;
 	char* fn;  
-	int cover[100000] = {0};
-
+	long long int cover[4690] = {0};
 	privcmd_hypercall_t set_cover={
 			__HYPERVISOR_set_cov_array,
 			{(long long int)&cover,0,0,0,0}
 	}; 
-//	privcmd_hypercall_t unset_cover={
-//			__HYPERVISOR_unset_cov_array,
-//			{0,0,0,0,0}
-//	};
 	fd = open("/proc/xen/privcmd",O_RDWR);
 	if (fd<0){
 			perror("cannot open privcmd");
@@ -33,24 +31,25 @@ int main(void){
 		printf("-------------failed!\n\n");
 		return 0;
 	}
+	
 	fn = "/dev/cov";
-	for (int i = 0; i < 10000; ++i)
-	{
-		if (cover[i] == '1')
-			printf("yes \t");
-	}
+//	for (int i = 0; i < 4690; ++i)
+//	{
+//		if (cover[i >> SHIFT] & (1 << (i & MASK)))
+//		printf("%lld \t", cover[i]);
+//	}
+	
 	covFd= open(fn, O_RDWR);
 	if (covFd == -1) {
 		creat(fn, 0777);
 		covFd = open(fn, O_RDWR);
 	}
+//	printf("\n\n %d \n\n", covFd);
+//	printf("\n\n %ld \n\n", sizeof(cover));
 	write(covFd, cover, sizeof(cover));
 	close(covFd);
-//	ret=ioctl(fd,IOCTL_PRIVCMD_HYPERCALL, &unset_cover); 
-//	if (ret < 0 )
-//		perror("no!");			
 	close(fd);
-    return 0;
+	return 0;
 
 }
 
